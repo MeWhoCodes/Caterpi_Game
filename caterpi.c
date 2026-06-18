d87932a215f09ef77555c2deab9de145f9b4e449
Devansh Sharma <devaton4@gmail.com>
2026-06-18 23:34:23 +0530

    function to add segments and display segments

--- caterpi.c
+++ caterpi.c
@@ -7,26 +7,54 @@
 #define spd 25
 #define sz  30
 
+
+struct seg{
+    char key;
+    int x;
+    int y;
+};
+
+void create_seg(struct seg *Aseg,int *len, int sx, int sy){
+    struct seg new_seg = {'_',sx,sy};
+    Aseg[*len] = new_seg;
+    *len = *len +1;
+}
+
+int disp_segs(struct seg *Aseg,int len, int x, int y){
+    for(int i=0;i<len;i++){
+        if(Aseg[i].x == y && Aseg[i].y == x){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
 int main() {
     SetConsoleOutputCP(65001); // for emojis;
-
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
     CONSOLE_CURSOR_INFO curs_info;
     GetConsoleCursorInfo(hConsole,&curs_info);
     curs_info.bVisible = FALSE;
     SetConsoleCursorInfo(hConsole,&curs_info);
-
     COORD topLeft = {0,0};
 
 
-    int exitas = 0;
- 
-    int buf_sz = 3*sz*sz + sz +1;
-   char key;
     
-    int bx=0;
-    int by = 0;
+
+    int exitas = 0;
+    int buf_sz = 3*sz*sz + sz +1;
+    char key;
+    
+    // int bx=0;
+    // int by = 0;
+
+    struct seg Arr_seg[sz*sz];
+    int Arr_len_inUse = 1; 
+    Arr_seg[0].key = key;
+    Arr_seg[0].x = 0;
+    Arr_seg[0].y = 0;
+    
 
     char *str= malloc(buf_sz);
 
@@ -36,6 +64,11 @@ int main() {
         if(kbhit()){ // for virtually locking the pressed key
             key = getch();
         }
+
+        for (int i = Arr_len_inUse - 1; i > 0; i--) { //fill what key needs to be used for each segment's movement
+            Arr_seg[i].key = Arr_seg[i - 1].key;
+        }
+        Arr_seg[0].key = key;
         
         str[0] = '\0';
         char *pointr = str;
@@ -59,9 +92,17 @@ int main() {
         for(int x =0;x<sz;x++){
             for(int y=0;y<sz;y++){
 
-                if(bx==y && by==x){
+                // if(bx==y && by==x){
+                //     strcpy(pointr,"\xE2\xAC\x9B");
+                //     pointr += 3;
+                // }else{
+                //     strcat(pointr,"\xE2\xAC\x9C");
+                //     pointr += 3;
+                // }
+                if(disp_segs(&Arr_seg,Arr_len_inUse,x,y)){
                     strcpy(pointr,"\xE2\xAC\x9B");
                     pointr += 3;
+
                 }else{
                     strcat(pointr,"\xE2\xAC\x9C");
                     pointr += 3;
